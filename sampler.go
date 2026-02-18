eaff00ba3d705a214598f3465036e850b6fd0c47
Konstantin Voykov <koykov0@gmail.com>
2026-02-18 23:44:45 +0300

    sampler: pass base inside sampler

--- sampler.go
+++ sampler.go
@@ -3,37 +3,39 @@ package lbpool
 import "math"
 
 const (
-	base             = 100000
+	base = 1e5
+
 	minReleaseFactor = 0.00001
 	maxReleaseFactor = 0.99999
 	epsilon          = 1e-9
 )
 
 type sampler struct {
+	base   uint64
 	lookup [base]bool
 }
 
-func newSampler(releaseFactor float64) *sampler {
+func newSampler(base uint64, releaseFactor float64) *sampler {
 	if releaseFactor < minReleaseFactor || math.IsNaN(releaseFactor) || math.IsInf(releaseFactor, 0) {
 		releaseFactor = 0 // all requests will pass
 	}
 	if releaseFactor > maxReleaseFactor {
 		releaseFactor = 1 // all requests will drop
 	}
-	s := &sampler{}
+	s := &sampler{base: base}
 	if releaseFactor == 0 {
 		return s
 	}
-	threshold := int(releaseFactor*base + epsilon)
-	if threshold > base {
-		threshold = base
+	threshold := uint64(releaseFactor*float64(s.base) + epsilon)
+	if threshold > s.base {
+		threshold = s.base
 	}
 	// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm implementation
-	var e int
-	for i := 0; i < base; i++ {
+	var e uint64
+	for i := uint64(0); i < s.base; i++ {
 		e += threshold
-		if e >= base {
-			e -= base
+		if e >= s.base {
+			e -= s.base
 			s.lookup[i] = true
 		}
 	}
@@ -41,5 +43,5 @@ func newSampler(releaseFactor float64) *sampler {
 }
 
 func (s *sampler) shouldDrop(i uint64) bool {
-	return s.lookup[i%base]
+	return s.lookup[i%s.base]
 }
